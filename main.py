c7c96aae298ba8bb17815c76c7d930553012c273
Alejandro Vargas <alevargasguitar777@gmail.com>
2022-07-26 20:03:55 -0500

    Templates and static files

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Buy coffe', 'Go to work', 'Make dinner']
+
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
 def hello():
-    return 'Hello World Flask'
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+
+    return render_template('hello.html', **context)
