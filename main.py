afd3593362a1b3f4e47d51cd54849be878732f57
Alejandro Vargas <alevargasguitar777@gmail.com>
2022-07-27 00:18:22 -0500

    Deploy flashes, POST and testing

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
-# Flask
-from flask import Flask, flash, request, make_response, redirect, render_template, session, url_for, flash
-
-# Bootstrap
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-
-# What the forms
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
-todos = ['Buy coffe', 'Go to work', 'Make dinner']
+
+todos = ['Buy coffe', 'Go to work', 'Do Excercise']
+
 
 class LoginForm(FlaskForm):
-    username = StringField('User Name', validators=[DataRequired()])
+    username = StringField('User name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+
 @app.errorhandler(404)
 def not_found(error):
-    """
-    Not Found
-    """
     return render_template('404.html', error=error)
+
 
 @app.errorhandler(500)
 def server_error(error):
-    """
-    Server Error
-    """
     return render_template('500.html', error=error)
+
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    # response.set_cookie('user_ip', user_ip)
     session['user_ip'] = user_ip
 
     return response
@@ -50,7 +48,6 @@ def index():
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    # user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
     login_form = LoginForm()
     username = session.get('username')
@@ -66,7 +63,7 @@ def hello():
         username = login_form.username.data
         session['username'] = username
 
-        flash('User Name Register Successfully!')
+        flash('Username Register Succesfully!')
 
         return redirect(url_for('index'))
 
