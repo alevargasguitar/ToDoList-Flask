ad8f59059ae6be54566f09d25ca27e5aef8e59db
Alejandro Vargas <alevargasguitar777@gmail.com>
2022-07-26 22:17:05 -0500

    Forms

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 # Flask
 from ensurepip import bootstrap
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 
 # Bootstrap
 from flask_bootstrap import Bootstrap
+
+# What the forms
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 todos = ['Buy coffe', 'Go to work', 'Make dinner']
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -30,17 +43,21 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    # response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
